f6d34f7cf5ecbe05bd0025050f564bb01cbfdae5
t.vanderkemp <t.vanderkemp@gouwit.nl>
2021-01-10 18:24:08 +0100

    Sparks rework

--- winterhold/src/main/java/winterhold/cards/destruction/Sparks.kt
+++ winterhold/src/main/java/winterhold/cards/destruction/Sparks.kt
@@ -1,31 +1,46 @@
 package winterhold.cards.destruction
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction
+import com.megacrit.cardcrawl.actions.animations.VFXAction
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction
+import com.megacrit.cardcrawl.actions.utility.SFXAction
 import com.megacrit.cardcrawl.cards.DamageInfo
 import com.megacrit.cardcrawl.characters.AbstractPlayer
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon
 import com.megacrit.cardcrawl.monsters.AbstractMonster
+import com.megacrit.cardcrawl.powers.VulnerablePower
+import com.megacrit.cardcrawl.vfx.combat.LightningEffect
 import winterhold.WinterholdMod
+import winterhold.actions.ComboAction
 import winterhold.actions.SpellDamageRandomEnemyAction
 import winterhold.spelldamage.SpellDamageTags
 import winterhold.spelldamage.SpellDamageType
+import winterhold.util.stillFightingMonsters
 
-class Sparks : AbstractDestructionCard(ID, IMG, COST, CardType.ATTACK, CardRarity.COMMON, CardTarget.ALL_ENEMY) {
-    override fun use(p: AbstractPlayer, m: AbstractMonster?) {
-        AbstractDungeon.actionManager.addToBottom(
+class Sparks : AbstractDestructionCard(ID, IMG, COST, CardType.ATTACK, CardRarity.COMMON, CardTarget.ENEMY) {
+    override fun use(p: AbstractPlayer, m: AbstractMonster) {
+        addToBot(SFXAction("THUNDERCLAP", 0.05F))
+        AbstractDungeon.getCurrRoom().monsters.stillFightingMonsters.forEach {
+            addToBot(VFXAction(LightningEffect(it.drawX, it.drawY), 0.05F))
+        }
+        addToBot(
             SpellDamageRandomEnemyAction(
                 DamageInfo(p, damage, damageTypeForTurn),
                 SpellDamageType.SHOCK,
                 AbstractGameAction.AttackEffect.NONE
             )
         )
+        addToBot(
+            ComboAction(
+                comboRequirement, ApplyPowerAction(m, p, VulnerablePower(m, magicNumber, false), magicNumber)
+            )
+        )
     }
 
-    //Upgraded stats.
     override fun upgrade() {
         if (!upgraded) {
             upgradeName()
-            upgradeDamage(UPGRADE_PLUS_DAMAGE)
+            upgradeComboToNewNumber(UPGRADE_NEW_COMBO)
         }
     }
 
@@ -33,13 +48,19 @@ class Sparks : AbstractDestructionCard(ID, IMG, COST, CardType.ATTACK, CardRarit
         val ID: String = WinterholdMod.makeID(Sparks::class.java.simpleName)
         val IMG: String = WinterholdMod.makeCardPath(Sparks::class.java)
 
-        private const val COST = 1
-        private const val DAMAGE = 8
-        private const val UPGRADE_PLUS_DAMAGE = 3
+        private const val COST = 0
+        private const val DAMAGE = 3
+        private const val COMBO = 3
+        private const val UPGRADE_NEW_COMBO = 2
+        private const val MAGIC_NUMBER = 1
     }
 
     init {
         baseDamage = DAMAGE
+        baseComboRequirement = COMBO
+        comboRequirement = COMBO
+        baseMagicNumber = MAGIC_NUMBER
+        magicNumber = MAGIC_NUMBER
         tags.add(SpellDamageTags.DEALS_SHOCK_DAMAGE)
     }
 }
